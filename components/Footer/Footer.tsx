e3544db4e9c2807cd8f3b0f6b07e99e4f002378d
Marco <serikbaev03beka@gmail.com>
2023-10-12 18:43:19 +0600

    Project init

---
+++ components/Footer/Footer.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Footer: React.FC = () => {
+  return <div className="bg-sky-900 p-6">Footer</div>;
+};
+
+export default Footer;
---
+++ components/Header/Header.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Header: React.FC = () => {
+  return <div className="bg-sky-900 p-6">Header</div>;
+};
+
+export default Header;
