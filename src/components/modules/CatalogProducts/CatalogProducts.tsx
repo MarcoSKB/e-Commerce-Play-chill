480d9dfaef80b01493712cc4b73d2735fc011746
marco <serikbaev03beka@gmail.com>
2023-10-20 19:56:21 +0600

    Fixed error CatalogProducts

--- src/components/modules/CatalogProducts/CatalogProducts.tsx
+++ src/components/modules/CatalogProducts/CatalogProducts.tsx
@@ -3,10 +3,10 @@ import React from "react";
 import { getGameAxios } from "@/src/api/getGameAxios";
 import useAxios from "@/src/hooks/useAxios";
 import { GamesDataResponse } from "@/src/types/GamesDataResponse";
-import { GamesList } from "@/src/components/elements";
+import { Container, GamesList } from "@/src/components/elements";
 
 const CatalogProducts = () => {
-  const { response, error, loading } = useAxios<GamesDataResponse>({
+  const [games, error, loading] = useAxios<GamesDataResponse>({
     axiosInstance: getGameAxios,
     method: "GET",
     url: "",
@@ -14,10 +14,12 @@ const CatalogProducts = () => {
 
   return (
     <div>
-      <h1 className="text-4xl">Product catalog</h1>
-      {response && (
-        <GamesList className="grid grid-cols-3" data={response.results} />
-      )}
+      <Container>
+        <h1 className="font-bold text-4xl mb-11">Product catalog</h1>
+        {games && (
+          <GamesList className="grid grid-cols-3" data={games.results} />
+        )}
+      </Container>
     </div>
   );
 };
