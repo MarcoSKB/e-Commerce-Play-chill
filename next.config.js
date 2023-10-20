4ca7c1938a6d58d36d73f27deef480e56362284c
marco <serikbaev03beka@gmail.com>
2023-10-20 20:17:26 +0600

    Fix error with api keys

--- next.config.js
+++ next.config.js
@@ -4,6 +4,9 @@ const nextConfig = {
   images: {
     domains: ["media.rawg.io"],
   },
+  env: {
+    NEXT_PUBLIC_RAWG_API_KEY: process.env.NEXT_PUBLIC_RAWG_API_KEY,
+  },
 };
 
 module.exports = nextConfig;
