d25d86a54c1a93c7cab9302db0a9817ea69e1da5
marco <serikbaev03beka@gmail.com>
2023-10-26 06:43:39 +0600

    Added background for advantage page

--- app/advantages/page.tsx
+++ app/advantages/page.tsx
@@ -1,9 +1,20 @@
-import React from "react";
+import Image from "next/image";
 
 const page = () => {
   return (
-    <main>
-      <h1>Our advantages</h1>
+    <main className="relative py-20 min-h-[1200px]">
+      <div className="absolute top-0 left-0 w-full h-full max-h-[670px] mix-blend-color-dodge overflow-hidden opacity-[0.95] -z-10">
+        <Image
+          priority
+          src="/images/bg-space.png"
+          className="w-full h-full object-cover"
+          alt="Background image"
+          width={1920}
+          height={1080}
+        />
+        <div className="absolute bottom-0 left-0 w-full h-[278px] advantages-gradient"></div>
+      </div>
+      <h1 className="text-[64px] font-extrabold">Our advantages</h1>
     </main>
   );
 };
