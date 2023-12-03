b3dfdfe6eedfbfe59154570dc0782cc57cb6bef2
Marco <serikbaev03beka@gmail.com>
2023-12-03 10:23:49 +0600

    Add TopGames component

--- app/page.tsx
+++ app/page.tsx
@@ -13,7 +13,7 @@ export default function Home() {
   return (
     <>
       <MainCarousel />
-      <Container className="flex flex-col gap-y-[80px] mb-20">
+      <Container className="flex flex-col gap-y-[80px] pt-[80px] mb-20">
         <TopGames />
         <Announcement />
         <TabsGameList />
--- src/components/modules/Section/Section.tsx
+++ src/components/modules/Section/Section.tsx
@@ -1,15 +1,15 @@
 import React from "react";
 
 interface Props {
-  title: string;
+  title: React.ReactNode;
   children: React.ReactNode;
   className?: string;
 }
 
 const Section: React.FC<Props> = (props) => {
-  const { title, className, children } = props;
+  const { title, className = "", children } = props;
   return (
-    <div className={`${className}`}>
+    <div className={className}>
       <h2 className="text-[32px] font-semibold mb-5">{title}</h2>
       {children}
     </div>
