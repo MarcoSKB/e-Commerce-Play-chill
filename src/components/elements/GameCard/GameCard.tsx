4335939890a7b53c696a8ae98da792483634f5f6
Marco <serikbaev03beka@gmail.com>
2023-12-04 19:13:35 +0600

    Change GameCard component

--- src/components/elements/GameCard/GameCard.tsx
+++ src/components/elements/GameCard/GameCard.tsx
@@ -1,10 +1,11 @@
 "use client";
-import { useState } from "react";
+import { MouseEvent, useState } from "react";
 import Image from "next/image";
 import Link from "next/link";
 
-import { IconButton } from "@/src/components/elements";
 import { Store } from "@/src/types/StoreType";
+import GameCardStore from "./GameCardStore";
+import GameCardAction from "./GameCardAction";
 
 interface Props {
   id: number;
@@ -18,52 +19,11 @@ interface Props {
 
 const GameCard: React.FC<Props> = (props) => {
   const { href, previewImageURL, price, title, store, label } = props;
-  const [isLike, setIsLike] = useState(false);
-  const [inCart, setInCart] = useState(false);
-
-  const onClickLikeHandler = (e: React.MouseEvent<HTMLButtonElement>) => {
-    e.preventDefault();
-    setIsLike(!isLike);
-  };
-
-  const onClickCartHandler = (e: React.MouseEvent<HTMLButtonElement>) => {
-    e.preventDefault();
-    setInCart(!inCart);
-    console.log("Clicked on cart");
-  };
-
-  const hasStorePlatform = (storesData: Store[], storePlatform: string) => {
-    const hasSteam = storesData?.find(
-      (stores) => stores.store.slug == storePlatform
-    );
-
-    return !!hasSteam;
-  };
 
   return (
     <Link href={`/products/${href}`} className="flex flex-col gap-5">
-      <div className="relative w-[300px] h-[400px] flex rounded-xl overflow-hidden">
-        {label && (
-          <div className="absolute top-0 left-0 flex justify-between items-start w-full h-full px-5 py-5 z-10">
-            {label}
-          </div>
-        )}
-        <div className="absolute top-1/2 w-full h-[calc(100%+300px)] flex flex-col justify-between items-end p-5 transition-all duration-300 -translate-y-1/2 hover:h-full z-20">
-          <IconButton
-            className={isLike ? "bg-green" : "bg-white"}
-            iconURL={isLike ? "/icons/heart-white.svg" : "/icons/heart-red.svg"}
-            iconAlt="Heart icon"
-            onClick={onClickLikeHandler}
-          />
-          <button
-            className="flex items-center justify-center w-full px-3 py-5 bg-green rounded-xl transition-all hover:scale-95 active:scale-90 disabled:active:scale-100 disabled:hover:scale-100 disabled:bg-gray-700"
-            disabled={inCart}
-            type="button"
-            onClick={onClickCartHandler}
-          >
-            To cart
-          </button>
-        </div>
+      <div className="relative w-full h-[400px] flex rounded-xl overflow-hidden">
+        <GameCardAction label={label} />
         <Image
           priority
           className="object-cover object-center"
@@ -78,26 +38,7 @@ const GameCard: React.FC<Props> = (props) => {
       <div className="flex flex-col gap-3 px-5">
         <div className="text-2xl">{price} $</div>
         <div className="text-base">{title}</div>
-        <div className="flex gap-5 flex-wrap">
-          {hasStorePlatform(store, "steam") && (
-            <div className="flex items-center gap-2 text-[#747474] text-sm font-medium">
-              <div className="w-[14px] h-[14px] rounded-full bg-[#3D394A]" />
-              Steam
-            </div>
-          )}
-          {hasStorePlatform(store, "gog") && (
-            <div className="flex items-center gap-2 text-[#747474] text-sm font-medium">
-              <div className="w-[14px] h-[14px] rounded-full bg-[#3D394A]" />
-              GOG
-            </div>
-          )}
-          {hasStorePlatform(store, "epic-games") && (
-            <div className="flex items-center gap-2 text-[#747474] text-sm font-medium">
-              <div className="w-[14px] h-[14px] rounded-full bg-[#3D394A]" />
-              Epic Games
-            </div>
-          )}
-        </div>
+        <GameCardStore store={store} />
       </div>
     </Link>
   );
---
+++ src/components/elements/GameCard/GameCardStore.tsx
@@ -0,0 +1,72 @@
+import { Store } from "@/src/types/StoreType";
+
+interface Props {
+  store: Store[];
+}
+
+const hasStoreData = [
+  {
+    id: 0,
+    storeSlug: "steam",
+    name: "Steam",
+  },
+  {
+    id: 1,
+    storeSlug: "gog",
+    name: "GOG",
+  },
+  {
+    id: 2,
+    storeSlug: "epic-games",
+    name: "Epic Games",
+  },
+];
+
+const GameCardStore: React.FC<Props> = ({ store }) => {
+  const hasStorePlatform = (storesData: Store[], storePlatform: string) => {
+    const hasSteam = storesData?.find(
+      (stores) => stores.store.slug == storePlatform
+    );
+    return !!hasSteam;
+  };
+
+  return (
+    <div className="flex gap-5 flex-wrap">
+      {hasStoreData.map(
+        (hasStore) =>
+          hasStorePlatform(store, hasStore.storeSlug) && (
+            <div
+              key={hasStore.id}
+              className="flex items-center gap-2 text-[#747474] text-sm font-medium"
+            >
+              <div className="w-[14px] h-[14px] rounded-full bg-[#3D394A]" />
+              {hasStore.name}
+            </div>
+          )
+      )}
+    </div>
+  );
+};
+
+export default GameCardStore;
+
+{
+  /* {hasStorePlatform(store, "steam") && (
+            <div className="flex items-center gap-2 text-[#747474] text-sm font-medium">
+              <div className="w-[14px] h-[14px] rounded-full bg-[#3D394A]" />
+              Steam
+            </div>
+          )}
+          {hasStorePlatform(store, "gog") && (
+            <div className="flex items-center gap-2 text-[#747474] text-sm font-medium">
+              <div className="w-[14px] h-[14px] rounded-full bg-[#3D394A]" />
+              GOG
+            </div>
+          )}
+          {hasStorePlatform(store, "epic-games") && (
+            <div className="flex items-center gap-2 text-[#747474] text-sm font-medium">
+              <div className="w-[14px] h-[14px] rounded-full bg-[#3D394A]" />
+              Epic Games
+            </div>
+          )} */
+}
