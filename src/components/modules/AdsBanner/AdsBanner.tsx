0e5f122d07424ef0ea0e39c5e1e0cfd3776d2e6a
Marco <serikbaev03beka@gmail.com>
2023-12-05 10:48:33 +0600

    Add AdsBanner component

--- src/components/modules/AdsBanner/AdsBanner.tsx
+++ src/components/modules/AdsBanner/AdsBanner.tsx
@@ -1,7 +1,15 @@
 import React from "react";
 
 const AdsBanner = () => {
-  return <div>AdsBanner</div>;
+  return (
+    <div className="relative w-full min-h-[150px] rounded-2xl overflow-hidden pointer-events-none">
+      <img
+        className="absolute w-full h-full object-cover"
+        src="/images/ads-banner.jpg"
+        alt="Advertisement"
+      />
+    </div>
+  );
 };
 
 export default AdsBanner;
