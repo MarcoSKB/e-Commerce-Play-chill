cb83d4d53698f12830d43bb837fbb2152605957d
Marco <serikbaev03beka@gmail.com>
2023-12-08 12:57:54 +0600

    Change AxiosInstances

--- src/api/getGameAxios.ts
+++ src/api/getGameAxios.ts
@@ -1,13 +1,7 @@
 import axios from "axios";
 
-const API_KEY = process.env.NEXT_PUBLIC_RAWG_API_KEY;
-const BASE_URL = `https://rawg.io/api/games`;
-
 export const getGameAxios = axios.create({
-  baseURL: BASE_URL,
-  params: {
-    key: API_KEY,
-  },
+  baseURL: "/api/games",
   headers: {
     "Content-Type": "application/json",
     Accept: "application/json",
--- src/api/rawgApiAxios.ts
+++ src/api/rawgApiAxios.ts
@@ -1,13 +1,9 @@
 import axios from "axios";
 
-const API_KEY = process.env.NEXT_PUBLIC_RAWG_API_KEY;
-const BASE_URL = `https://rawg.io/api`;
+const BASE_URL = `/api/rawg`;
 
 export const rawgApiAxios = axios.create({
   baseURL: BASE_URL,
-  params: {
-    key: API_KEY,
-  },
   headers: {
     "Content-Type": "application/json",
     Accept: "application/json",
