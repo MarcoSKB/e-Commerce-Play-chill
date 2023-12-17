afdb93714e97bd02c3bf774e6124be3da21e58dc
Marco <serikbaev03beka@gmail.com>
2023-12-17 22:34:33 +0600

    Fix db error

--- src/libs/auth.ts
+++ src/libs/auth.ts
@@ -34,7 +34,7 @@ export const authOptions: NextAuthOptions = {
     async jwt({ token, user }) {
       const dbUser = await db.user.findFirst({
         where: {
-          email: token.email,
+          email: token.email!,
         },
       });
 
