5469671e7bc0e4b54f2f9af8c7b3a6aa37684b64
Marco <serikbaev03beka@gmail.com>
2023-12-17 22:17:34 +0600

    Add CreateReview component

---
+++ src/components/modules/Reviews/CreateReview.tsx
@@ -0,0 +1,106 @@
+"use client";
+import { useState } from "react";
+import { SubmitHandler, useForm } from "react-hook-form";
+import { toast } from "sonner";
+import axios from "axios";
+
+import { UserImage } from "@/src/components/elements";
+import RatingRadioInput from "./RatingRadioInput";
+
+export interface IFormInputs {
+  comment: string;
+  rating: string;
+}
+
+interface Props {
+  gameId: number | undefined;
+  setUpdateReviewList: () => void;
+}
+
+const CreateReview: React.FC<Props> = ({ gameId, setUpdateReviewList }) => {
+  const [rating, setRating] = useState("0");
+  const {
+    reset,
+    register,
+    setValue,
+    handleSubmit,
+    formState: { errors, isLoading },
+  } = useForm<IFormInputs>({
+    defaultValues: {
+      rating: "0",
+    },
+  });
+
+  const handlerRadioInput = (value: string) => setRating(value);
+
+  const onSubmit: SubmitHandler<IFormInputs> = async (data) => {
+    try {
+      reset();
+      handlerRadioInput("0");
+
+      await axios.post("/api/reviews", {
+        gameId: gameId,
+        rating: +data.rating,
+        content: data.comment,
+      });
+
+      setUpdateReviewList();
+      return toast.success("Review succesfully added");
+    } catch (error) {
+      if (axios.isAxiosError(error)) {
+        return toast.error("There was a problem", {
+          description: error.response?.data.error,
+        });
+      }
+
+      return toast.error("There was a problem", {
+        description: "Please try later",
+      });
+    }
+  };
+
+  return (
+    <div className="flex items-start mb-12">
+      <UserImage className="mx-8" width={48} />
+      <form
+        className="grid grid-cols-2 gap-8 w-full"
+        onSubmit={handleSubmit(onSubmit)}
+      >
+        <label className="flex flex-col gap-4">
+          <h3
+            className={`text-xl font-semibold ${
+              errors.comment ? "text-rose-500" : ""
+            }`}
+          >
+            Your comment {errors.comment && "*"}
+          </h3>
+          <textarea
+            {...register("comment", {
+              required: true,
+              minLength: 5,
+              maxLength: 196,
+            })}
+            className="h-[124px] py-3 px-4 rounded-xl bg-darkPurple resize-none placeholder:opacity-20 outline-none focus-visible:outline-blue"
+            placeholder="Type your comment here..."
+            maxLength={196}
+          />
+        </label>
+        <RatingRadioInput
+          setValue={setValue}
+          register={register}
+          errors={errors}
+          setRating={handlerRadioInput}
+          rating={rating}
+        />
+        <input
+          type="submit"
+          value="Submit comment"
+          disabled={gameId === undefined || isLoading}
+          className="py-3 px-8 bg-green rounded-xl w-fit text-lg font-bold transition-transform hover:scale-95 visited:scale-90 focus:outline-none focus:outline-blue disabled:bg-gray-600 disabled:hover:scale-100"
+        />
+      </form>
+    </div>
+  );
+};
+
+export default CreateReview;
---
+++ src/components/modules/Reviews/RatingRadioInput.tsx
@@ -0,0 +1,116 @@
+import { ChangeEvent, useEffect, useState } from "react";
+import {
+  FieldErrors,
+  UseFormGetValues,
+  UseFormRegister,
+  UseFormSetValue,
+} from "react-hook-form";
+import { IFormInputs } from "./CreateReview";
+
+interface Props {
+  setValue: UseFormSetValue<IFormInputs>;
+  setRating: (value: string) => void;
+  rating: string;
+  register: UseFormRegister<IFormInputs>;
+  errors: FieldErrors<IFormInputs>;
+}
+
+const RatingRadioInput: React.FC<Props> = (props) => {
+  const { setValue, setRating, rating, register, errors } = props;
+
+  const handleRadioChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setValue("rating", e.target.value);
+    setRating(e.target.value);
+  };
+
+  return (
+    <div className="flex flex-col gap-4">
+      <h3
+        className={`text-xl font-semibold ${
+          errors.rating ? "text-rose-500" : ""
+        }`}
+      >
+        Select rating {errors.rating && "*"}
+      </h3>
+      <div className="flex gap-1 px-5 py-3 w-fit rounded-lg border border-white border-solid border-opacity-10">
+        <label
+          className={`relative w-6 h-[23px] bg-cover transition-transform origin-center hover:scale-90 star ${
+            +rating >= 1
+              ? "bg-[url('/icons/star/star.svg')]"
+              : "bg-[url('/icons/star/star-empty.svg')]"
+          }`}
+        >
+          <input
+            {...register("rating", { required: true })}
+            className="absolute w-full h-full opacity-0"
+            onChange={handleRadioChange}
+            type="radio"
+            value={1}
+          />
+        </label>
+        <label
+          className={`relative w-6 h-[23px] bg-cover transition-transform origin-center hover:scale-90 star ${
+            +rating >= 2
+              ? "bg-[url('/icons/star/star.svg')]"
+              : "bg-[url('/icons/star/star-empty.svg')]"
+          }`}
+        >
+          <input
+            {...register("rating", { required: true })}
+            className="absolute w-full h-full opacity-0"
+            onChange={handleRadioChange}
+            type="radio"
+            value={2}
+          />
+        </label>
+        <label
+          className={`relative w-6 h-[23px] bg-cover transition-transform origin-center hover:scale-90 star ${
+            +rating >= 3
+              ? "bg-[url('/icons/star/star.svg')]"
+              : "bg-[url('/icons/star/star-empty.svg')]"
+          }`}
+        >
+          <input
+            {...register("rating", { required: true })}
+            className="absolute w-full h-full opacity-0"
+            onChange={handleRadioChange}
+            type="radio"
+            value={3}
+          />
+        </label>
+        <label
+          className={`relative w-6 h-[23px] bg-cover transition-transform origin-center hover:scale-90 star ${
+            +rating >= 4
+              ? "bg-[url('/icons/star/star.svg')]"
+              : "bg-[url('/icons/star/star-empty.svg')]"
+          }`}
+        >
+          <input
+            {...register("rating", { required: true })}
+            className="absolute w-full h-full opacity-0"
+            onChange={handleRadioChange}
+            type="radio"
+            value={4}
+          />
+        </label>
+        <label
+          className={`relative w-6 h-[23px] bg-cover transition-transform origin-center hover:scale-90 star ${
+            +rating >= 5
+              ? "bg-[url('/icons/star/star.svg')]"
+              : "bg-[url('/icons/star/star-empty.svg')]"
+          }`}
+        >
+          <input
+            {...register("rating", { required: true })}
+            className="absolute w-full h-full opacity-0"
+            onChange={handleRadioChange}
+            type="radio"
+            value={5}
+          />
+        </label>
+      </div>
+    </div>
+  );
+};
+
+export default RatingRadioInput;
