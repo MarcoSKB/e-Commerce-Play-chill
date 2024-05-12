aee29f70f4bafec60f5e98f91215684bc177a566
Marco <serikbaev03beka@gmail.com>
2024-05-12 08:48:25 +0500

    Add nodemailer

--- app/api/email/route.ts
+++ app/api/email/route.ts
@@ -1,15 +1,25 @@
 import { NextRequest } from "next/server";
+import nodemailer from "nodemailer";
 import { emailValidator } from "@/src/libs/emailValidator";
-import sgMail from "@sendgrid/mail";
 
-sgMail.setApiKey("SG." + process.env.SENDGRID_API_KEY!);
+const APIkey = process.env.SENDGRID_API_KEY;
 
 export const POST = async (req: NextRequest) => {
   try {
     const body = await req.json();
     const { email, subject, html } = emailValidator.parse(body);
 
-    sgMail.send({
+    const transporter = nodemailer.createTransport({
+      host: "smtp.sendgrid.net",
+      port: 465,
+      secure: true,
+      auth: {
+        user: "apikey",
+        pass: APIkey,
+      },
+    });
+
+    await transporter.sendMail({
       from: "serikbaev03beka@gmail.com",
       to: email,
       subject: subject,
