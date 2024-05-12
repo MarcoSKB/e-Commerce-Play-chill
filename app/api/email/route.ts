c4722ec5c036a8bc823b90e185a555d54dbb2232
Marco <serikbaev03beka@gmail.com>
2024-05-12 05:57:55 +0500

    Add SendGrid

--- app/api/email/route.ts
+++ app/api/email/route.ts
@@ -1,45 +1,20 @@
 import { NextRequest } from "next/server";
-import nodemailer from "nodemailer";
+import { Resend } from "resend";
 import { emailValidator } from "@/src/libs/emailValidator";
+import sgMail from "@sendgrid/mail";
 
-const EMAIL_USER = process.env.EMAIL_USER;
-const EMAIL_PASSWORD = process.env.EMAIL_PASSWORD;
+sgMail.setApiKey(process.env.SENDGRID_API_KEY!);
 
 export const POST = async (req: NextRequest) => {
   try {
     const body = await req.json();
     const { email, subject, html } = emailValidator.parse(body);
 
-    const mailOptions = {
-      from: "Marco <Marco.skb@mail.ru>",
+    sgMail.send({
+      from: "serikbaev03beka@gmail.com",
       to: email,
       subject: subject,
       html: html,
-    };
-
-    const transporter = nodemailer.createTransport({
-      host: "smtp.mail.ru",
-      port: 25,
-      secure: true,
-      auth: {
-        user: EMAIL_USER,
-        pass: EMAIL_PASSWORD,
-      },
-      tls: {
-        rejectUnauthorized: false,
-      },
-    });
-
-    await new Promise((resolve, reject) => {
-      transporter.sendMail(mailOptions, (err, info) => {
-        if (err) {
-          console.error(err);
-          reject(err);
-        } else {
-          console.log(info);
-          resolve(info);
-        }
-      });
     });
 
     return Response.json(
