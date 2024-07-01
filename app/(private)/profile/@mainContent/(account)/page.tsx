1497d8f228964279c493014a88270f2fd73210e7
Marco <serikbaev03beka@gmail.com>
2024-07-01 22:02:44 +0500

    Add auth validation for main profile page

--- app/(private)/profile/@mainContent/(account)/page.tsx
+++ app/(private)/profile/@mainContent/(account)/page.tsx
@@ -1,5 +1,15 @@
-import React from "react";
+"use client";
+import useAuthSession from "@/src/hooks/useAuthSession";
 
 export default function page() {
+  const [session, loading] = useAuthSession();
+
+  if (loading) {
+    return <>Loading</>;
+  }
+
+  if (session == null) {
+    return <>null</>;
+  }
   return <div>Account page</div>;
 }
