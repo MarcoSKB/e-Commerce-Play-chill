f283e48167c6dbefa431aa843c2da658c390ec1a
Marco <serikbaev03beka@gmail.com>
2024-07-05 21:10:18 +0500

    Add middleware instead of client side fetch session

--- app/(private)/profile/@mainContent/(account)/page.tsx
+++ app/(private)/profile/@mainContent/(account)/page.tsx
@@ -1,14 +1,3 @@
-// import useAuthSession from "@/src/hooks/useAuthSession";
-
 export default function page() {
-  // const [session, loading] = useAuthSession();
-
-  // if (loading) {
-  //   return <>Loading</>;
-  // }
-
-  // if (session == null) {
-  //   return <>null</>;
-  // }
   return <div>Account page</div>;
 }
