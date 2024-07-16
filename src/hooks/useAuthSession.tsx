9ef1436a641229c1eb30d1265aa69ca2d4dac2b1
Marco <serikbaev03beka@gmail.com>
2024-07-17 02:42:43 +0500

    Add parameter for useAuthSession hook

--- src/hooks/useAuthSession.tsx
+++ src/hooks/useAuthSession.tsx
@@ -2,11 +2,12 @@ import { Session } from "next-auth";
 import useAxios from "./useAxios";
 import { authAxios } from "../api/authAxios";
 
-export default function useAuthSession() {
+export default function useAuthSession(dependency?: unknown[]) {
   const [session, _, loading] = useAxios<Session>({
     url: "/session",
     method: "GET",
     axiosInstance: authAxios,
+    dependency,
   });
   return [session, loading] as const;
 }
