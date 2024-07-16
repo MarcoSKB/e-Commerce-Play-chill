4c2191a9522f1455a4ee4f0439cc33aef5ad4f81
Marco <serikbaev03beka@gmail.com>
2024-07-17 02:44:25 +0500

    Add loading state for button

--- app/(private)/profile/components/elements/ProfileButton/ProfileButton.tsx
+++ app/(private)/profile/components/elements/ProfileButton/ProfileButton.tsx
@@ -1,4 +1,6 @@
 "use client";
+import { LoadingSpin } from "@/src/components/ui";
+
 interface Props {
   children: React.ReactNode;
   onClick?: () => void;
@@ -6,18 +8,22 @@ interface Props {
   color?: "red" | "green";
   /** @defaultValue 'button' */
   type?: "submit" | "button";
+  /** @defaultValue 'false' */
+  loading?: boolean;
 }
 
 const ProfileButton: React.FC<Props> = (props) => {
-  const { children, onClick, color, type = "button" } = props;
+  const { children, onClick, color, type = "button", loading = false } = props;
 
   if (color == "red") {
     return (
       <button
         type={type}
         onClick={onClick}
-        className="w-full py-[8px] px-5 font-medium text-center text-red bg-black border-2 border-black rounded-lg transition-all hover:border-red active:scale-95"
+        disabled={loading}
+        className="flex justify-center items-center gap-[2px] w-full py-[8px] px-5 font-medium text-red bg-black border-2 border-black rounded-lg transition-all hover:border-red active:scale-95 disabled:pointer-events-none disabled:text-white disabled:text-opacity-30 disabled:border-[#211E2A]"
       >
+        {loading && <LoadingSpin color="#211E2A" width={20} height={20} />}
         {children}
       </button>
     );
@@ -27,8 +33,10 @@ const ProfileButton: React.FC<Props> = (props) => {
     <button
       type={type}
       onClick={onClick}
-      className="w-full py-[8px] px-5 font-medium text-center text-green border-2 border-green rounded-lg transition-all hover:bg-green hover:text-black active:scale-95"
+      disabled={loading}
+      className="flex justify-center items-center gap-[2px] w-full py-[8px] px-5 font-medium text-green border-2 border-green rounded-lg transition-all hover:bg-green hover:text-black active:scale-95 disabled:pointer-events-none disabled:text-white disabled:text-opacity-30 disabled:border-[#211E2A]"
     >
+      {loading && <LoadingSpin color="#211E2A" width={22} height={22} />}
       {children}
     </button>
   );
